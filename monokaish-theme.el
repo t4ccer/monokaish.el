270b9c70a6d3b2a2569a007a3566b170cce42812
t4ccer <t4ccer@gmail.com>
2022-09-17 22:23:54 -0600

    fix

--- monokaish-theme.el
+++ monokaish-theme.el
@@ -1,5 +1,26 @@
 ;;; monokaish-theme.el --- A monokaish color theme for Emacs -*- lexical-binding: t -*-
 
+;; Copyright (C) 2022 Tomasz Maciosowski (t4ccer)
+
+;; Author: Tomasz Maciosowski <t4ccer@gmail.com>
+;; Maintainer: Tomasz Maciosowski <t4ccer@gmail.com>
+;; Version: 0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'autothemer)
 
 (autothemer-deftheme
@@ -150,6 +171,15 @@
   (magit-diff-removed-highlight      (:inherit    'magit-diff-removed))
   (magit-diff-added-highlight        (:inherit    'magit-diff-added))
   ))
+
+
+;;;###autoload
+(when (and (boundp 'custom-theme-load-path)
+           load-file-name)
+  (add-to-list 'custom-theme-load-path
+               (file-name-as-directory (file-name-directory load-file-name)))
+  (add-to-list 'load-path
+	       (file-name-as-directory (file-name-directory load-file-name))))
 
 (provide-theme 'monokaish)
 
